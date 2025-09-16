a3521140b7094d7949b87e9e5976918f69b67e8b
Stingos <Justmo10@gmail.com>
2025-09-16 01:41:48 -0400

    Add GitHub Pages deployment workflow

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,38 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      // Set the public path for GitHub Pages
+      publicPath: isProduction ? '/Todo-list/' : '/',
+    },
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    devServer: {
+      watchFiles: ["./src/template.html"],
+      hot: true,
+      open: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        favicon: false, // Add a favicon later if you want
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+  };
 };
